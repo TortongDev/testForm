e9f4433ed30c5d0699200ff3f351ae3bcfbcb640
kittithat <kittithat.charoenkhet101@gmail.com>
2022-05-18 11:02:25 +0700

    edit index add form  add style

--- index.php
+++ index.php
@@ -3,10 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
 <body>
-    
+    <div class="wrapper-page">
+        <section class="wrapper-form">
+            <form action="" method="post">
+                <div class="form-grp">
+                    <label for="name">Name</label>                    
+                    <input type="text" name="name">
+                </div>
+                <div class="form-grp">
+                    <label for="name">Name</label>                    
+                    <input type="text" name="name">
+                </div>
+                
+            </form>
+        </section>
+    </div>
 </body>
 </html>
