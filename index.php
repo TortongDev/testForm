79aa53d420c09fdc99e1fa4539e2ad3b448f7b8d
kittithat <kittithat.charoenkhet101@gmail.com>
2022-05-19 11:31:53 +0700

    edit

--- index.php
+++ index.php
@@ -3,60 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Form Input</title>
 </head>
 <body>
-    <div class="wrapper-page">
-        <?php 
-        require_once "connection.php";
-            $stmt = $conn->query("select * from product");
-            $stmt->execute();
-
-            $result = $stmt->fetch(PDO::FETCH_OBJ)
-        ?>
-
-        <table width="40%">
-            <tr>
-                <th>name</th>
-                <th>dated</th>
-                <th>name_01</th>
-                <th>price</th>
-            </tr>
-            
-            <tr>
-                <td><?php echo $result->name; ?></td>
-                <td><?php echo $result->dated; ?></td>
-                <td><?php echo $result->name_01; ?></td>
-                <td><?php echo $result->price; ?></td>
-            </tr>
-           
-        </table>
+    <div class="wrapper">
         <section class="wrapper-form">
-            <form action="" method="post">
+            <form action="insert.php" method="post">
                 <div class="form-grp">
-                    <label for="name">Name</label>                    
-                    <input type="text" name="name">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="form-input">
                 </div>
                 <div class="form-grp">
-                    <label for="dated">Name</label>                    
-                    <input type="text" name="dated">
+                    <label for="dates">Date</label>
+                    <input type="date" name="dates" class="form-input">
                 </div>
                 <div class="form-grp">
-                    <label for="name_01">Name</label>                    
-                    <input type="text" name="name_01">
+                    <label for="name_01">Name Save</label>
+                    <input type="text" name="name_01" class="form-input">
                 </div>
                 <div class="form-grp">
-                    <label for="price">Name</label>                    
-                    <input type="text" name="price">
+                    <label for="price">Price</label>
+                    <input type="text" name="price" class="form-input">
                 </div>
-                <div class="form-grp">
-                    <label for=""></label>
-                    <button type="submit" name="insert">save</button>
+                <div class="form-btn">
+                    <button type="submit" name="insert">Save</button>
                 </div>
-                
             </form>
         </section>
     </div>
--- insert.php
+++ insert.php
@@ -1,16 +1,20 @@
 <?php
     require_once "connection.php";
-    $arr_data = array(
-        $_POST['name'],
-        $_POST['dated'],
-        $_POST['name_01'],
-        $_POST['price']
-    );
-    $stmt = $conn->prepare("insert into product(name,dated,name_01,price) values (?,?,?,?)");
-    $stmt->execute($arr_data);
-    if($stmt):
-        echo json_encode(array('status'=>'success'));
-    else:
-        echo json_encode(array('status'=>'False'));
-    endif;
+
+    try {
+        $data = array(
+            $_POST['name'],$_POST['dates'],$_POST['name_01'],$_POST['price']
+        );
+        print_r($data);
+        if(isset($_POST['insert'])):
+            $stmt = $conn->prepare('insert into product(name,dated,name_01,price) values (?,?,?,?)');
+            $stmt->execute($data);
+            header("Location: success.html");
+        else:
+            echo "Insert False.";
+        endif;
+
+    } catch (PDOException $err) {
+    
+    }
 ?>
